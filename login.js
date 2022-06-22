e06660a587d4b6b8f595ce8ee0e4bada32bdc322
RanjuRaniKarna <88411930+RanjuRaniKarna@users.noreply.github.com>
2022-06-22 16:55:51 -0500

    Add files via upload

---
+++ login.js
@@ -0,0 +1,9 @@
+function login(){
+    var player1 = document.getElementById("player1_name").value ;
+    var player2 = document.getElementById("player2_name").value ;
+    console.log("player1 = " + player1);
+    console.log("player2 = " + player2);
+    localStorage.setItem("player1", player1);
+    localStorage.setItem("player2", player2);
+    window.location="game_page.html";
+}
